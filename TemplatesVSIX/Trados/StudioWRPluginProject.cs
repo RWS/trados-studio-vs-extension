2b2b549006df84b6896200096d7b8fbc7fee0ee0
imoldovan <imoldovan@sdl.com>
2022-02-02 16:24:38 +0200

    update

---
+++ TemplatesVSIX/Trados/StudioWRPluginProject.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using TemplatesVSIX.Trados.Patches;
+
+namespace TemplatesVSIX.Trados
+{
+    public class StudioWRPluginProject
+    {
+        private readonly StudioPluginProject _project;
+        
+        public StudioWRPluginProject(string projectFile)
+        {
+            _project = new StudioPluginProject(projectFile,
+                new TargetFrameworkPatch("v4.8"),
+                new HintPathPatch("17"),
+                new DeploymentPathPatch("17"),
+                new PluginFrameworkPatch("2.0.0", "16.0.1")
+            );
+        }
+
+        public Task<bool> Update()
+        {
+            return _project.Update();
+        }
+    }
+}
