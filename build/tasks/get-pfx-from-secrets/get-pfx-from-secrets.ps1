d25103bb7cbc2f52768f19aceb5397a3abec5e8b
imoldovan <imoldovan@sdl.com>
2022-02-07 19:40:45 +0200

    update

--- build/tasks/get-pfx-from-secrets/get-pfx-from-secrets.ps1
+++ build/tasks/get-pfx-from-secrets/get-pfx-from-secrets.ps1
@@ -43,5 +43,5 @@ else
 }
 $ToolPath=[System.IO.Path]::GetFullPath("C:\Users\VssAdministrator\.nuget\packages\Microsoft.VSSDK.Vsixsigntool\16.2.29116.78\tools\vssdk\vsixsigntool.exe");
 
-& $ToolPath sign /f $pfxPath /sha1 "<sha1 bytes>" /p $password /fd sha1 $fileToSign
+& $ToolPath sign /v /f $pfxPath  /p $password /fd sha1 $fileToSign
 
