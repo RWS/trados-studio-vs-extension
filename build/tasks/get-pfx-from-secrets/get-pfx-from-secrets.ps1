550ea46dc4e95a03b4a36b390314aa192ae1c77a
imoldovan <imoldovan@sdl.com>
2022-02-07 16:55:17 +0200

    add get-pfx-from-secrets

---
+++ build/tasks/get-pfx-from-secrets/get-pfx-from-secrets.ps1
@@ -0,0 +1,40 @@
+#copy the password and pfx from azure keyvault variables.
+
+$pfxPath = "$psscriptroot\..\..\..\tools\SDLPLCAuthenticode.pfx"
+
+if ($null -eq $env:TF_BUILD)
+{
+    #if we are running locally then we can generate a pfx.
+    if (!(test-path $pfxPath))
+    {
+        $cert = New-SelfSignedCertificate -Subject "SDL Internal Codesigning" -Type CodeSigningCert -CertStoreLocation cert:\LocalMachine\My
+        #Move-Item -Path $cert.PSPath -Destination "Cert:\CurrentUser\Root"
+
+        $CertPassword = ConvertTo-SecureString -String "unset" -Force -AsPlainText
+
+        Export-PfxCertificate -Cert $cert -FilePath "$pfxPath" -Password $CertPassword
+
+        write-host "Created $pfxPath"
+
+        $env:SDLPLCAuthenticodeKey = "unset"
+    }
+}
+else
+{
+    #get certificate from keyvault as we are running under a server build.
+    $pfxSecretStringValue=$env:SDLPLCAuthenticode
+    Write-Host "pfxSecretStringValue=$pfxSecretStringValue"
+
+    $kvSecretBytes = [System.Convert]::FromBase64String($pfxSecretStringValue)
+    $certCollection = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2Collection
+    $certCollection.Import($kvSecretBytes, $null, [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::Exportable)
+
+
+    $password = $env:SDLPLCAuthenticodeKey
+    $protectedCertificateBytes = $certCollection.Export([System.Security.Cryptography.X509Certificates.X509ContentType]::Pkcs12, $password)
+    
+    [System.IO.File]::WriteAllBytes($pfxPath, $protectedCertificateBytes)
+    write-host "Extracted $pfxPath from Azure keyvault"
+}
+
+
