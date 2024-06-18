612f511cc30898869f57d9c08e4d7483bf3a9ad0
Ionel-Cristian Moldovan <66057010+icmoldovan@users.noreply.github.com>
2024-06-18 12:12:05 +0300

    - update nugets and versions (#30)
    Co-authored-by: Robert Cornea <rcornea@sdl.com>

--- TemplatesVSIX/Trados/StudioWRPluginProject.cs
+++ TemplatesVSIX/Trados/StudioWRPluginProject.cs
@@ -3,23 +3,23 @@ using TemplatesVSIX.Trados.Patches;
 
 namespace TemplatesVSIX.Trados
 {
-    public class StudioWRPluginProject
-    {
-        private readonly StudioPluginProject _project;
-        
-        public StudioWRPluginProject(string projectFile)
-        {
-            _project = new StudioPluginProject(projectFile,
-                new TargetFrameworkPatch("v4.8"),
-                new ReferencePatch("17"),
-                new DeploymentPathPatch("17"),
-                new PluginFrameworkPatch("2.1.0", "17.0.0")
-            );
-        }
+	public class StudioWRPluginProject
+	{
+		private readonly StudioPluginProject _project;
 
-        public Task<bool> Update()
-        {
-            return _project.UpdateAsync();
-        }
-    }
+		public StudioWRPluginProject(string projectFile)
+		{
+			_project = new StudioPluginProject(projectFile,
+				new TargetFrameworkPatch("v4.8"),
+				new ReferencePatch("18"),
+				new DeploymentPathPatch("18"),
+				new PluginFrameworkPatch("2.1.0", "18.0.1")
+			);
+		}
+
+		public Task<bool> Update()
+		{
+			return _project.UpdateAsync();
+		}
+	}
 }
