3c19e247dade2fe7b81a083a4b287ff29f417721
Robert Cornea <rcornea@sdl.com>
2024-06-18 12:02:43 +0300

    - update nugets and versions

--- TemplatesVSIX/Trados/Patches/ReferencePatch.cs
+++ TemplatesVSIX/Trados/Patches/ReferencePatch.cs
@@ -1,44 +1,43 @@
 ﻿using System.Linq;
-using System.Text.RegularExpressions;
 using TemplatesVSIX.MsBuild;
 
 namespace TemplatesVSIX.Trados.Patches
 {
-    internal class ReferencePatch : IStudioPluginPatch
-    {
-        private readonly string _newVersion;
+	internal class ReferencePatch : IStudioPluginPatch
+	{
+		private readonly string _newVersion;
 
-        public ReferencePatch(string newVersion)
-        {
-            _newVersion = newVersion;
-        }
+		public ReferencePatch(string newVersion)
+		{
+			_newVersion = newVersion;
+		}
 
-        public void PatchProject(IProject project)
-        {
-            project?.References
-                .Where(r => r.Include.Name.ToLower().Contains("sdl") || r.HintPath.ToLower().Contains("sdl"))
-                ?.ToList()
-                .ForEach(UpdateHintPath);
-        }
+		public void PatchProject(IProject project)
+		{
+			project?.References
+				.Where(r => r.Include.Name.ToLower().Contains("sdl") || r.HintPath.ToLower().Contains("sdl"))
+				?.ToList()
+				.ForEach(UpdateHintPath);
+		}
 
-        public void PatchPackages(IPackagesConfig packageConfig)
-        {
-        }
+		public void PatchPackages(IPackagesConfig packageConfig)
+		{
+		}
 
-        private void UpdateHintPath(IReference reference)
-        {
-            if (reference.HintPath.Contains("PluginFramework"))
-            {
-                reference.DeleteReference();
-            }
-            else
-            {
-                reference.HintPath =
-                    $@"$(MSBuildProgramFiles32)\Trados\Trados Studio\Studio{_newVersion}\{reference.Include.Name}.dll";
+		private void UpdateHintPath(IReference reference)
+		{
+			if (reference.HintPath.Contains("PluginFramework"))
+			{
+				reference.DeleteReference();
+			}
+			else
+			{
+				reference.HintPath =
+					$@"$(MSBuildProgramFiles32)\Trados\Trados Studio\Studio{_newVersion}\{reference.Include.Name}.dll";
 
-                reference.DeleteElement("Private");
-                reference.DeleteElement("SpecificVersion");
-            }
-        }
-    }
+				reference.DeleteElement("Private");
+				reference.DeleteElement("SpecificVersion");
+			}
+		}
+	}
 }
