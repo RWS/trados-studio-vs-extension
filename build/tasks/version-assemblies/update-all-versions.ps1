c56eed66c06e9a7ec87403ef2c4a834150daf497
David Watson - RWS <90462285+rwsdwatson@users.noreply.github.com>
2024-08-01 17:59:33 +0300

    LG-45180 fix signing (#31)
    * LG-45180 update signing
    
    * use dotnet sign
    
    * use environment for auth
    
    * fix versioning
    
    * FIx icons

--- build/tasks/version-assemblies/update-all-versions.ps1
+++ build/tasks/version-assemblies/update-all-versions.ps1
@@ -6,12 +6,15 @@ $VSIXFile="$PSScriptRoot\..\..\..\TemplatesVsix\source.extension.vsixmanifest"
 
 $VSIXFile=[System.IO.Path]::GetFullPath($VSIXFile);
 
+$VSIXID="6D87597D-63A9-4A1C-BB39-A970AD731ED1"
+
 write-output "Updating version:"
 write-output "`$VSIXFile=$VSIXFile"
 write-output "`$VersionString=$VersionString"
+write-output "`$VSIXID=$VSIXID"
 
 
 
-& "$PSScriptRoot\..\regex-replace-in-file\regex-replace-in-file.ps1" "$VSIXFile" "Id=`"3D390539-4A5C-4A0E-AAF8-2BCD8EA83FF1`" Version=`".*`" L" "Id=`"3D390539-4A5C-4A0E-AAF8-2BCD8EA83FF1`" Version=`"$VersionString`" L" 0 65001
+& "$PSScriptRoot\..\regex-replace-in-file\regex-replace-in-file.ps1" "$VSIXFile" "Id=`"$VSIXID`" Version=`".*`" L" "Id=`"$VSIXID`" Version=`"$VersionString`" L" 0 65001
 
 
